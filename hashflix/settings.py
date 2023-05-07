bde4cdd06c7238f995dec4b4ce4ad8bba57baeea
Josyway <107008079+Josyway@users.noreply.github.com>
2023-05-07 10:32:54 -0300

    ajustes csrf

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -31,7 +31,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://projetonetflix-production.up.railway.app", "localhost", "127.0.0.1/"]
+ALLOWED_HOSTS = ["projetonetflix-production.up.railway.app", "localhost", "127.0.0.1/"]
 
 
 # Application definition
